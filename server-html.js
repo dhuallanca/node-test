1662f68b3bfdcb10dae87a458ed00292edf155d7
unknown <dzavaleta@hotmail.com>
2019-05-19 17:18:34 -0500

    add handler bars and heroku

--- server-html.js
+++ server-html.js
@@ -1,7 +1,30 @@
 const express = require('express');
 
 const app = express();
+const hbs = require('hbs');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/publico'));
+hbs.registerPartials(__dirname + '/views/parciales');
+//return handlerbar hbs view
+app.set('view engine', 'hbs');
 
-app.listen(3000);
+// helper
+require('./hbs/helpers');
+
+app.get('/', (req, res) => {
+    // renderisa el home.hbs
+    res.render('home', {
+        nombre: 'Dennis',
+        anio: new Date()
+    });
+});
+
+app.get('/about', (req, res) => {
+    // renderisa el home.hbs
+    res.render('about', {
+        nombre: 'Dennis'
+    });
+});
+
+app.listen(port);
